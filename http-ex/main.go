c7bd7c63a2e52a229f69a9f8797abac4b60fc59b
Yar83Men <yarik83.evpat@gmail.com>
2026-05-16 13:46:47 +0300

    Рефакторинг

--- http-ex/main.go
+++ http-ex/main.go
@@ -11,6 +11,28 @@ import (
 )
 
 func main() {
+	city, err := getCity()
+	weatherResponse, err := weather.GetWeather(city)
+	if err != nil {
+		errorHandler()
+		return
+	}
+	if weatherResponse == nil {
+		errorHandler()
+		return
+	}
+	color.Yellow("Погда в городе:" + city + "\n")
+	color.Yellow("Температура:" + getTemperature(weatherResponse.Current.TempC) + "\n")
+	color.Magenta("Время суток:" + timesOfDay(weatherResponse.Current.IsDay) + "\n")
+	color.Red("Давление:" + strconv.FormatFloat(weatherResponse.Current.PressureMb, 'g', -1, 64) + "\n")
+	color.Cyan("Вероятность облачности:" + strconv.Itoa(weatherResponse.Current.Cloud) + "\n")
+	color.Blue("Вероятность дождя:" + strconv.Itoa(weatherResponse.Current.WillItRain) + "\n")
+
+	color.Cyan("По истечению 7-ми сек. закроется")
+	time.Sleep(7000 * time.Millisecond)
+}
+
+func getCity() (string, error) {
 	var city string
 	color.Green("Введите название города, например 'Moscow' или 'Eupatoria'")
 	_, err := fmt.Scanln(&city)
@@ -18,33 +40,33 @@ func main() {
 		color.Red(err.Error())
 	}
 	city = strings.ToUpper(city[:1]) + strings.ToLower(city[1:])
+	return city, err
+}
 
-	weatherResponse, err := weather.GetWeather(city)
-	if err != nil {
-		color.Red("Ошибка определения локации")
-		time.Sleep(3000 * time.Millisecond)
-		return
-	}
+func errorHandler() {
+	color.Red("Ошибка определения локации, зашел не в ту дверь)")
+	time.Sleep(3000 * time.Millisecond)
+}
 
-	if weatherResponse == nil {
-		color.Red("Ошибка определения локации")
-		time.Sleep(3000 * time.Millisecond)
-		return
-	}
+func timesOfDay(isDay int) string {
 	var dayOrNight string
-	if weatherResponse.Current.IsDay == 0 {
-		dayOrNight = "ночь"
+	if isDay == 0 {
+		dayOrNight = "Мойте ноги ложитесь спать)"
 	} else {
-		dayOrNight = "день"
+		dayOrNight = "Солнце светит негры пашут)"
 	}
+	return dayOrNight
+}
 
-	color.Yellow("Погда в городе:" + city + "\n")
-	color.Yellow("Температура:" + strconv.FormatFloat(weatherResponse.Current.TempC, 'g', -1, 64) + "\n")
-	color.Magenta("Время суток:" + dayOrNight + "\n")
-	color.Red("Давление:" + strconv.FormatFloat(weatherResponse.Current.PressureMb, 'g', -1, 64) + "\n")
-	color.Cyan("Вероятность облачности:" + strconv.Itoa(weatherResponse.Current.Cloud) + "\n")
-	color.Blue("Вероятность дождя:" + strconv.Itoa(weatherResponse.Current.WillItRain) + "\n")
-
-	color.Cyan("По истечению 7-ми сек. закроется")
-	time.Sleep(7000 * time.Millisecond)
+func getTemperature(temp float64) string {
+	var result string
+	temperatureStr := strconv.FormatFloat(temp, 'g', -1, 64)
+	if temp < 20 {
+		result += "Дабачина, как в холодильнике " + temperatureStr + "\n"
+	} else if temp > 20 && temp < 30 {
+		result += "Прохладно, норм " + temperatureStr + "\n"
+	} else {
+		result += "Шкварит как в микроволновке " + temperatureStr + "\n"
+	}
+	return result
 }
